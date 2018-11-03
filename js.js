700dc92c2fab76277c212281e409cc5c26805129
trunglk16 <trungnguyenlk16@gmail.com>
2018-11-03 16:56:00 +0700

    sua bai toan ve tinh chu vi hinh tron

--- js.js
+++ js.js
@@ -4,5 +4,4 @@ r = prompt("Nhap ban kinh hinh tron");
 c = 2 * pi * r;
 s = pi * r * r;
 document.write("Chu vi hinh tron = " + c);
-document.write("<br/>");
-document.write("Dien tich hinh tron = " + s );
+
